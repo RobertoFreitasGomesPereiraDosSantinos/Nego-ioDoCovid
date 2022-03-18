a3edf507da883b3dc244be7b1eeb3329adc2c516
RobertoFreitasGomesPereiraDosSantinos <99353140+RobertoFreitasGomesPereiraDosSantinos@users.noreply.github.com>
2022-03-18 16:27:20 -0300

    Add files via upload

---
+++ escripito.js
@@ -0,0 +1,139 @@
+function bt01() {
+    let clk = 4;
+    switch (clk) {
+        case 4 : {
+            let div = document.getElementById("quadro");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("quadro").innerHTML = "tome ômega 3, regina";
+        }
+        break;
+        case 3 : {
+            let div = document.getElementById("quadro");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("quadro").innerHTML = "como assim? Tem tanto ar  no mundo";
+        }
+        break;
+        case 4 : {
+            let div = document.getElementById("quadro");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("quadro").innerHTML = "taca sal na lingua";
+        }
+        break;
+        case 4 : {
+            let div = document.getElementById("quadro");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("quadro").innerHTML = "espero que você morra =)";
+        }
+        break;
+        default : console.log("deu merda");
+    }
+}
+
+function bt02() {
+    let clk = 3;
+    switch (clk) {
+        case 4 : {
+            let div = document.getElementById("quadro");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("quadro").innerHTML = "tome ômega 3, regina";
+        }
+        break;
+        case 3 : {
+            let div = document.getElementById("quadro");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("quadro").innerHTML = "como assim? Tem tanto ar  no mundo";
+        }
+        break;
+        case 4 : {
+            let div = document.getElementById("quadro");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("quadro").innerHTML = "taca sal na lingua";
+        }
+        break;
+        case 4 : {
+            let div = document.getElementById("quadro");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("quadro").innerHTML = "espero que você morra =)";
+        }
+        break;
+        default : console.log("deu merda");
+    }
+}
+
+function bt03() {
+    let clk = 2;
+    switch (clk) {
+        case 4 : {
+            let div = document.getElementById("quadro");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("quadro").innerHTML = "tome ômega 3, regina";
+        }
+        break;
+        case 3 : {
+            let div = document.getElementById("quadro");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("quadro").innerHTML = "como assim? Tem tanto ar  no mundo";
+        }
+        break;
+        case 2 : {
+            let div = document.getElementById("quadro");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("quadro").innerHTML = "taca sal na lingua";
+        }
+        break;
+        case 1 : {
+            let div = document.getElementById("quadro");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("quadro").innerHTML = "espero que você morra =)";
+        }
+        break;
+        default : console.log("deu merda");
+    }
+}
+
+function bt04() {
+    let clk =1;
+    switch (clk) {
+        case 4 : {
+            let div = document.getElementById("quadro");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("quadro").innerHTML = "tome ômega 3, regina";
+        }
+        break;
+        case 3 : {
+            let div = document.getElementById("quadro");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("quadro").innerHTML = "como assim? Tem tanto ar  no mundo";
+        }
+        break;
+        case 2 : {
+            let div = document.getElementById("quadro");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("quadro").innerHTML = "taca sal na lingua";
+        }
+        break;
+        case 1 : {
+            let div = document.getElementById("quadro");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("quadro").innerHTML = "espero que você morra =)";
+        }
+        break;
+        default : console.log("deu merda");
+    }
+}
